dbb49c4029928d17bf71b31311af87593536c0d5
Abondance <kazadiabondance@gmail.com>
2025-04-10 14:04:48 +0200

    send last code

--- backend/src/controllers/useQuotes.js
+++ backend/src/controllers/useQuotes.js
@@ -1,10 +1,36 @@
-const quotes = require("../data/quotes");
+const fs = require("fs");
+const path = require("path");
+
+const filePath = path.join(__dirname, "../data/quotes.json");
 
 module.exports.getAllQuotes = async (req, res) => {
   try {
-    const datas = await quotes;
-    return res.status(200).json(datas);
+    const data = fs.readFileSync(filePath, "utf8");
+    const quotes = JSON.parse(data);
+
+    const random = Math.floor(Math.random() * quotes.length);
+    return res.status(200).json(quotes[random]);
   } catch (error) {
     return res.status(500).json({ error: "Erreur serveur" });
+  }
+};
+
+module.exports.createQuote = async (req, res) => {
+  try {
+    const { content, author } = req.body;
+
+    const data = fs.readFileSync(filePath, "utf8");
+    const quotes = JSON.parse(data);
+
+    const id = quotes.length + 1;
+    const newQuote = { id, content, author };
+
+    quotes.push(newQuote);
+
+    fs.writeFileSync(filePath, JSON.stringify(quotes, null, 2));
+
+    return res.status(201).json({ message: "Quotes add with success" });
+  } catch (error) {
+    return res.status(500).json({ error: "Server error" });
   }
 };
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getAllQuotes } = require("../controllers/useQuotes");
+const { getAllQuotes, createQuote } = require("../controllers/useQuotes");
 
 const router = express.Router();
 
 router.get("/api/quotes", getAllQuotes);
+router.post("/api/quote", createQuote);
 
 module.exports = router;
